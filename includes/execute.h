ab9369ef426875f7898e83fcace8c6c3c007bf1e
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-11-04 20:46:31 +0100

    created execute.h

---
+++ includes/execute.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 20:00:20 by mamichal          #+#    #+#             */
+/*   Updated: 2024/11/04 20:45:57 by mamichal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXECUTE_H
+# define EXECUTE_H
+
+# include "minishell.h"
+
+# ifndef PATH_LEN
+#	define PATH_MAX 4096
+# endif
+
+#endif
