616c1085676eec9f61b3505d306484f5e73b1fb8
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-11-04 20:47:56 +0100

    added ft_pwd, to be tested

--- src/builtins/ft_pwd.c
+++ src/builtins/ft_pwd.c
@@ -6,7 +6,20 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 15:27:19 by mamichal          #+#    #+#             */
-/*   Updated: 2024/10/31 15:27:22 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/11/04 20:47:45 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "execute.h"
+
+int	ft_pwd(void)
+{
+	// TODO: TESTS
+	char cwd[PATH_MAX];
+	if (NULL == getcwd(cwd, PATH_MAX))
+	{
+		perror("minishell pwd");
+		return (EXIT_FAILURE);
+	}
+	return (printf("%s", cwd), EXIT_SUCCESS);
+}
