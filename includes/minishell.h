ca7b824432fb96b968b5c0258d6e5fcdbdf7d7cd
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-11-04 20:46:18 +0100

    added header files to minishell.h

---
+++ includes/minishell.h
@@ -0,0 +1,21 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <bits/waitflags.h>
+# include <dirent.h>
+# include <errno.h>
+# include <stdbool.h>
+# include <limits.h>
+# include "libft.h"
+
+#endif
