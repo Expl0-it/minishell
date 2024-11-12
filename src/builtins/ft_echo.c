e0a9d24ef21f0bb70656cf482c9834e19e01df9a
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-11-12 13:12:25 +0100

    added ft_echo, to be tested

---
+++ src/builtins/ft_echo.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_echo.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 14:00:04 by mamichal          #+#    #+#             */
+/*   Updated: 2024/11/12 13:10:23 by mamichal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "execute.h"
+
+static void	echo_args(const char **args, bool n_flag, int i)
+{
+	if (NULL != args || NULL != *args)
+	{
+		printf("%s", args[i]);
+		i++;
+		while (args[i])
+		{
+			printf(" %s", args[i]);
+			i++;
+		}
+	}
+	if (false == n_flag)
+		printf("\n");
+}
+
+int	ft_echo(const char **args)
+{
+	// TODO: TESTS
+	bool	n_flag;
+	int		i;
+
+	n_flag = false;
+	i = 1;
+	if (NULL == args || NULL == *args)
+		return (EXIT_FAILURE);
+	if (NULL != args[i] && ft_strncmp(args[i], "-n", 3) == 0)
+	{
+		n_flag = true;
+		i++;
+	}
+	echo_args(args, n_flag, i);
+	return (EXIT_SUCCESS);
+}
