28fa39de420635f6344c38c4b7201c1ead98d500
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-11-27 19:53:41 +0100

    removed brackets on single line else statment

--- env/init_env.c
+++ env/init_env.c
@@ -35,9 +35,7 @@ t_env	*new_env_node(char **split)
 	if (NULL == split[2])
 		new_node->val = ft_strdup(split[1]);
 	else
-	{
 		new_node->val = concat_split(split, '=', 1);
-	}
 	if (NULL == new_node->key || NULL == new_node->val)
 	{
 		free(new_node->key);
