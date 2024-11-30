28614fe98f7bba6ac3e7ce0f80def13aaebf057b
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-11-30 16:49:11 +0000

    added new commands to parse.c

--- parse/parse.c
+++ parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 18:50:45 by rdavurov          #+#    #+#             */
-/*   Updated: 2024/11/28 12:36:11 by codespace        ###   ########.fr       */
+/*   Updated: 2024/11/28 20:43:57 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,34 +18,36 @@ void	skip_spaces(char **input)
 		(*input)++;
 }
 
-int		char_count(char **input)
+int		char_count(char *input, int *i)
 {
 	int		len;
 
 	len = 0;
-	while (*(*input) != ' ' && *(*input) != '\0')
+	while (input[*i] != ' ' && input[*i] != '\0')
 	{
-		if (*(*input) == '"' || *(*input) == '\'')
+		if  (input[*i] == '"' || input[*i] == '\'')
 		{
-			(*input)++;
-			while (*(*input) != '"' && *(*input) != '\'')
+			(*i)++;
+			while (input[*i] != '"' && input[*i] != '\'')
 			{
 				len++;
-				(*input)++;
+				(*i)++;
 			}
-			(*input)++;
-			skip_spaces(input);
+			(*i)++;
+			while (input[*i] == ' ')
+				(*i)++;
 			return len;
 		}
 		else
 		{
 			len++;
-			(*input)++;
-			if (*(*input) == '\'' || *(*input) == '"')
+			(*i)++;
+			if (input[*i] == '\'' || input[*i] == '"')
 				break;
 		}
 	}
-	skip_spaces(input);
+	while (input[*i] == ' ')
+		(*i)++;
 	return len;
 }
 
@@ -82,21 +84,61 @@ int		count_commands(char *input)
 	return (count);
 }
 
+
+// loop over argv with iterators ))))))))))))))
+void	fill_cmd(char **input, char **argv)
+{
+	while (**input != ' ' && **input != '\0')
+	{
+		if (**input == '"' || **input == '\'')
+		{
+			(*input)++;
+			**argv = **input;
+			(*argv)++;
+			while (**input != '"' && **input != '\'')
+			{
+				**argv = **input;
+				(*argv)++;
+				(*input)++;
+			}
+			(*input)++;
+		}
+		else
+		{
+			**argv = **input;
+			(*argv)++;
+			(*input)++;
+		}
+		skip_spaces(input);
+		**argv = '\0';
+		argv++;
+	}
+}
+
 void	parse_input(t_data *data, char *input)
 {
 	int		count;
-	int i;
+	int 	i;
+	int		j;
 	
 	i = 0;
+	j = 0;
 	count = count_commands(input);
 	data->argv = (char **)malloc(sizeof(char *) * count + 1);
 	
 	skip_spaces(&input);
 	while (i < count)
 	{
-		data->argv[i] = (char *)malloc(sizeof(char) * char_count(&input) + 1);
-		// put the string into the array :))))))))))))))))
-		data->argv[i][char_count(&input)] = '\0';
+		data->argv[i] = (char *)malloc(sizeof(char) * char_count(input, &j) + 1);
+		i++;
+	}
+	fill_cmd(&input, data->argv);
+
+	// print data->argv
+	i = 0;
+	while (data->argv[i] != NULL)
+	{
+		printf("data->argv[%d] = %s\n", i, data->argv[i]);
 		i++;
 	}
 }
