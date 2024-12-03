f82412ed8f22d41b9d12bdd193f79bda86c17ef8
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-12-03 11:46:11 +0000

    added init_env

--- main.c
+++ main.c
@@ -6,22 +6,24 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 18:34:14 by rdavurov          #+#    #+#             */
-/*   Updated: 2024/11/12 20:49:22 by codespace        ###   ########.fr       */
+/*   Updated: 2024/12/03 10:39:16 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main()
+int	main(int ac, char **av, char **envp)
 {
 	t_data	data;
 	char	*input;
 
+	((void)ac, (void)av);
 	while (1)
 	{
 		input = readline("minishell$ ");
 		if (*input != '\0')
 			add_history(input);
+		init_env(&data, envp);
 		parse_input(&data, input);
 		free(input);
 	}
