083de9aa5c97a5d962f53d5cdac3a93ca7efa21e
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-12-04 11:42:54 +0100

    prototyped new main for tommorow meeting

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 18:34:14 by rdavurov          #+#    #+#             */
-/*   Updated: 2024/12/03 12:10:40 by codespace        ###   ########.fr       */
+/*   Updated: 2024/12/04 11:42:38 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,35 @@ int	main(int ac, char **av, char **envp)
 	}
 	return (0);
 }
+
+/* NOTE: Prototyped new main
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_data	data;
+	char	*line;
+
+	((void)argc, (void)argv);
+	if (EXIT_FAILURE == init_env(&data, envp))
+		exit(EXIT_FAILURE);
+	while (true)
+	{
+		//handle signals
+		line = readline("[minishell]$");
+		if (NULL == line)
+			break;
+		if (!*line)
+		{
+			free(line);
+			continue ;
+		}
+		add_history(line);
+		parse_input(&data, line);
+		free(line);
+		// TODO: check pipe & execute
+		// if (true == check_pipe)
+		//		execute(&data);
+		// cleanup
+	}
+}
+*/
