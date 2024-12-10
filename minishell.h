0cc7435f682b12f967817daa03503a3220023d72
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-12-10 12:55:18 +0100

    added pid to s_pipe struct

--- minishell.h
+++ minishell.h
@@ -46,6 +46,7 @@ typedef struct s_pipe
 	char			*limiter; // when using heredoc: << limiter || else NULL
 	int				infd; // when redirecting input < infile
 	int				outfd; // when redirecting output > outfile
+	pid_t			pid; // current proccess pid
 	bool			here_doc; // do we use heredoc (input redirection "<<")
 	bool			invalid_infile; // does the infile exist and do we have premissions to it?
 	t_write_mode	write_mode; // do we use replace ">" or append ">>" file's contents with output redirection
