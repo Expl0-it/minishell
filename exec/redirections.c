50f54c09fffeb827d4d56f14690d2ae2e02ed06f
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-01-02 12:25:05 +0100

    added redirect input

--- exec/redirections.c
+++ exec/redirections.c
@@ -1,5 +1,20 @@
 #include "minishell.h"
 
+static void	redirect_input(t_data *data, int i)
+{
+	t_pipes *curr;
+
+	curr = &data->pipes[i];
+	if (true == curr->heredoc && NULL != curr->limiter)
+		handle_heredoc(curr);
+	else if (0 == curr->pid)
+	{	
+		if (curr->infd != -1)
+			dup2(curr->infd, STDIN_FILENO);
+	}
+	else
+		dup2(curr->fds[0], STDIN_FILENO);
+}
 
 static void	redirect_output(t_data *data, int i)
 {
