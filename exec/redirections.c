50d4a7ee544088a3fc2e8513443d33f9f0b845bc
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-01-02 12:24:48 +0100

    added redirect output

--- exec/redirections.c
+++ exec/redirections.c
@@ -1,5 +1,18 @@
 #include "minishell.h"
 
+
+static void	redirect_output(t_data *data, int i)
+{
+	t_pipes *curr;
+
+	curr = &data->pipes[i];
+	if (-1 != curr->outfd)
+		dup2(curr->outfd, STDOUT_FILENO);
+	else
+		dup2(curr->fds[1], STDOUT_FILENO);
+
+}
+
 void	handle_redirections(t_data *data, int i)
 {
 	redirect_input(data, i);
