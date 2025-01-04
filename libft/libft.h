3a05a77a5556a16d50dad041b1929f4c080efb72
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2025-01-04 21:58:55 +0000

    update libft header file

--- libft/libft.h
+++ libft/libft.h
@@ -704,5 +704,5 @@ void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 //additional
-
+char *ft_strndup(const char *s1, size_t n);
 #endif
