e21153a278028349d80f3d40e8b80085f1bac36c
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-01-04 19:38:33 +0100

    added fn to get an absolute path fron data.env

---
+++ exec/paths.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paths.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/04 16:15:26 by mamichal          #+#    #+#             */
+/*   Updated: 2025/01/04 19:38:00 by mamichal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*get_absolute_path(t_data *data, char *cmd_path)
+{
+	char	*abs_path;
+	char	*cwd;
+	char	*tmp;
+
+	if ('.' == cmd_path[0])
+	{
+		cwd = get_env_val(data->env, "PWD");
+		tmp = ft_substr(cmd_path, 1, ft_strlen(cmd_path) - 1);
+		abs_path = ft_strjoin(cwd, tmp);
+		free(tmp);
+	}
+	else
+		abs_path = ft_strdup(cmd_path);
+	return (abs_path);
+}
+
