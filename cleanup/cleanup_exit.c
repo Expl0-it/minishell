1446c74ebc150436faab352f34b9e95bc623896b
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-01-23 12:05:29 +0100

    added a blueprint for cleanup fns

---
+++ cleanup/cleanup_exit.c
@@ -0,0 +1,8 @@
+
+#include "minishell.h"
+
+// NOTE: it will be the cleanup on program exit (ctrl+D / exit command)
+void	cleanup_exit(t_data *data)
+{
+
+}
---
+++ cleanup/cleanup_req.c
@@ -0,0 +1,7 @@
+#include "minishell.h"
+
+// NOTE: It will be the cleanup on every single request
+void	cleanup_req(t_data *data)
+{
+
+}
