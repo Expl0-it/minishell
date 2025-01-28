592197f52ead0c6e30f36d5a964a3cb119ab2ec0
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-01-28 12:16:15 +0100

    used data struct instead of reference in pipes.c

--- exec/pipes.c
+++ exec/pipes.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 14:31:12 by mamichal          #+#    #+#             */
-/*   Updated: 2025/01/28 11:45:47 by mamichal         ###   ########.fr       */
+/*   Updated: 2025/01/28 12:15:35 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,11 @@
 void	open_pipes(t_data *data)
 {
 	int		i;
-	t_pipes	*curr;
 
 	i = 0;
 	while (NULL != data->pipes && NULL != data->pipes[i].cmd)
 	{
-		curr = &data->pipes[i];
-		if (pipe(curr->fds) < 0)
+		if (pipe(data->pipes[i].fds) < 0)
 			err_exit(data, "pipe failure\n", PIPE_ERR);
 		i++;
 	}
@@ -31,31 +29,27 @@ void	open_pipes(t_data *data)
 void	close_pipes(t_data *data)
 {
 	int		i;
-	t_pipes	*curr;
 
 	i = 0;
 	while (NULL != data->pipes && NULL != data->pipes[i].cmd)
 	{
-		curr = &data->pipes[i];
-		close(curr->fds[0]);
-		close(curr->fds[1]);
+		close(data->pipes[i].fds[0]);
+		close(data->pipes[i].fds[1]);
 		i++;
 	}
 }
 
 void	close_useless_pipes(t_data *data, int i_pipe)
 {
-	t_pipes	*pipes;
 	int		i_curr;
 
-	pipes = data->pipes;
 	i_curr = 0;
-	while (NULL != pipes && NULL != pipes[i_curr].cmd)
+	while (NULL != data->pipes && NULL != data->pipes[i_curr].cmd)
 	{
 		if (i_pipe != i_curr)
-			close(pipes[i_curr].fds[1]);
+			close(data->pipes[i_curr].fds[1]);
 		if (i_pipe - 1 != i_curr)
-			close(pipes[i_curr].fds[0]);
+			close(data->pipes[i_curr].fds[0]);
 		i_curr++;
 	}
 }
