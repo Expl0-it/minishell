6095e0249e720ccbad456c57c36aa8c16809cbf3
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2025-01-31 20:19:32 +0000

    handle heredoc

---
+++ lexing/helpers.c
@@ -0,0 +1,36 @@
+#include "minishell.h"
+
+void set_append(char **args, int j, t_pipes *pipe)
+{
+	int flag;
+
+	flag = O_TRUNC;
+	if (ft_strlen(args[j]) > 2)
+		return;
+	if (!ft_strncmp(args[j], ">>", 3))
+	{
+		flag = O_APPEND;
+		pipe->write_mode = APPEND;
+	}
+	if (pipe->outfile)
+	{
+		close(pipe->outfd);
+		free(pipe->outfile);
+	}
+	pipe->outfd = open(args[j + 1], O_WRONLY | O_CREAT | flag, 0644);
+	pipe->outfile = ft_strdup(args[j + 1]);
+}
+
+void set_heredoc(t_pipes *pipe, char *arg)
+{
+	pipe->limiter = ft_strdup(arg);
+	pipe->heredoc = true;
+}
+
+void set_redirection(char **args, int j, t_pipes *pipe)
+{
+	pipe->infd = open(args[j + 1], O_RDONLY);
+	if (pipe->infile)
+		close(pipe->infd);
+	pipe->infile = ft_strdup(args[j + 1]);
+}
