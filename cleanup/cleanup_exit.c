152a28da1ecb3073694a5b4e34dca9abd2b162e5
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-02-13 12:13:30 +0100

    norminette

--- cleanup/cleanup_exit.c
+++ cleanup/cleanup_exit.c
@@ -6,13 +6,12 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 16:07:52 by mamichal          #+#    #+#             */
-/*   Updated: 2025/02/12 16:07:53 by mamichal         ###   ########.fr       */
+/*   Updated: 2025/02/13 12:13:15 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// NOTE: it will be the cleanup on program exit (ctrl+D / exit command)
 void	cleanup_exit(t_data *data)
 {
 	free_env(data->env);
